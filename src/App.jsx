25092041c0fa50a49fd8699bf5167cb35d2e619c
unforgitful <dunkfunker@gmail.com>
2025-04-28 14:45:32 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,77 @@
+import { useState } from 'react'
+import questions from './Questions.jsx' //contains the questionnaires
+import victory from './assets/yippie.gif'
+import defeat from './assets/you-lost.webp'
+import wsound from './assets/wsound.mp3'
+import lsound from './assets/lsound.mp3'
+
+
+function App() {
+
+  const [score, setScore] = useState (0);
+  const [showScore, setShowScore] = useState(false);
+  const [currentQuestion, setCurrentQuestion] = useState(0); //0 is the default index for the first question.
+  
+
+  const handleAnswerOption = (selectedAnswer) => {
+    selectedAnswer === questions[currentQuestion].correctAnswer && setScore(score + 1);
+
+
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setShowScore(true);
+      score == questions.length -1 && setshowText(true);
+    }
+
+  }
+  return (
+    <>
+      <h1>Short Quiz App</h1>
+      {showScore ? (
+        <div className="score-section">
+          You scored {score} out of {questions.length}
+          {score >= 3 ?(
+            <div>
+              <img src={victory} alt="Victory!" />
+              {/* Did not remove/hide audio controls for user adjustments. */}
+             <audio src={wsound} autoPlay  />
+              <p>You passed the quiz! Yippie!</p>
+            </div>
+          ) : (
+            <div>
+              <img src={defeat} alt="Defeat..." />
+              {/* Did not remove/hide audio controls for user adjustments. */}
+              <audio src={lsound} autoPlay />
+              <p>Better luck next time!</p>
+            </div>
+          )}
+        </div>
+      ):
+      <>
+      <div className="question-container">
+        <div className="question-num">
+          <span>Question {currentQuestion + 1}</span> / {questions.length}
+        </div>
+        <div className="question-text">{questions[currentQuestion].question}</div>
+        <div className="question-answers">{questions[currentQuestion].options.map((option) =>
+          (
+            <button 
+            className="answer-button" 
+            key={option} 
+            onClick={() => handleAnswerOption(option)}
+            >
+        {option}
+        </button>))}</div>
+      </div>
+      </>
+      
+      }
+
+
+    </>
+  )
+}
+
+export default App
---
+++ src/Questions.jsx
@@ -0,0 +1,35 @@
+
+const questions = [
+    {
+        question: "Which color is the mixture of red and blue?",
+        options: ["Teal", "Purple", "Orange", "Green"],
+        correctAnswer: "Purple",
+    },
+    {
+        question: "What is the largest ocean on earth?",
+        options: ["Atlantic Ocean", "Indian Ocean", "Pacific Ocean", "Southern Ocean"],
+        correctAnswer: "Pacific Ocean",
+    },
+    {
+        question: "When did World War 1 end?",
+        options: ["November 17, 1910", "September 11, 1918", "March 8, 1922", "November 11, 1918"],
+        correctAnswer: "November 11, 1918",
+    },
+    {
+        question: "Whick King had the Knights of the Round Table?",
+        options: ["Alexander The Great", "Genghis Khan", "Arthur Pendragon", "Charlemagne"],
+        correctAnswer: "Arthur Pendragon",
+    },
+    {
+        question: "Who discovered electricty?",
+        options: ["Benjamin Franklin", "Nikola Tesla", "Michael Faraday", "Thomas Edison"],
+        correctAnswer: "Benjamin Franklin",
+    },
+    {
+        question: "How many planets are in our Solar System?",
+        options: ["12", "8", "6", "9"],
+        correctAnswer: "8",
+    },
+];
+
+export default questions
